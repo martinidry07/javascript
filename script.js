4a71cdab8ab9f67269113c938a38a9ef5b133437
martinidry07 <70594234+martinidry07@users.noreply.github.com>
2020-09-25 21:26:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-console.log ("Second file");
+/*console.log ("Second file");
 
 var x;
 x= 123;
 let y = "text";
-const z = 5; /* mainīgais, kuru nevarēs izmainīt*/
+const z = 5; /* mainīgais, kuru nevarēs izmainīt
 
 message();
 function message(m) {
     console.log(x + y);
     console.log(x + z);
 }   
+*/
 
-let output = document.getElementById('output');
-let button = document.createElement("button");
-button.textContent="Texts ko likt pogā";
-output.append(button);
-console.log(output);
+
+
+
+addbutton(0);
+function addbutton(i){
+    let output = document.getElementById('output'); 
+    let button = document.createElement("button");
+    i=i+1;
+    button.textContent="Button " + i;
+    output.append(button);
+    console.log(output);
+    if(i<42){
+        addbutton (i);
+    }
+
+}
